9a9669115f4297c364371a71c372e85ed875d4cc
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-03-12 19:40:28 +0100

    initialize Vite and React setup with TypeScript; remove unused scripts and files

--- scripts/loading.js
+++
@@ -1,5 +0,0 @@
-const url = new URL(window.location.href);
-if (url.searchParams.get('s')) {
-	localStorage.setItem('NextPauseChoice', url.searchParams.get('s').replaceAll('%20', ''));
-	window.location.href = url.origin + url.pathname;
-}
--- scripts/share.js
+++
@@ -1,7 +0,0 @@
-$('#share').click(function () {
-    navigator.clipboard.writeText(`${window.location.href}?s=${encodeURI(localStorage.getItem("NextPauseChoice"))}`);
-    $('h6').show();
-    setTimeout(() => {
-        $('h6').hide();
-    }, 2000);
-});
